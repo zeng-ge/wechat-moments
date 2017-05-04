b9ca20e2abd292f1b41f34ccaf6224652e172519
zeng ge <gzeng@CNgzeng.local>
2017-05-04 15:41:48 +0800

    init app

---
+++ app/src/main/java/zg/org/wechat_moments/MomentsActivity.java
@@ -0,0 +1,13 @@
+package zg.org.wechat_moments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MomentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moments);
+    }
+}
